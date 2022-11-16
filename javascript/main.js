655639a9b341db595a5f1109fe2a922033676ba1
Ttst49 <thibautstachnick@gmail.com>
2022-11-16 14:43:21 +0100

    optimisation and usable case

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,10 @@
 const newMessage= document.querySelector('#message')
 const boutonEnvoyer = document.querySelector('.envoi')
 const requetePost = new XMLHttpRequest();
-const chatButton = document.querySelector('.chat')
 const signUpButton = document.querySelector('.signUp')
-const connexionButton= document.querySelector('.connexion')
+const chatButton = document.querySelector('.chat')
+
+
 bodyPage = document.querySelector('body')
 fil= document.querySelector('.filAccueil')
 
@@ -12,7 +13,6 @@ let nom=''
 let id=''
 let contenu=''
 
-//onload= "refreshPage(5000);"
 
 let templateChat=`
     <div class="container page">
@@ -30,21 +30,125 @@ let templateChat=`
     </div>
 `
 
+let templateSignUp=`
+<div class="container page">
+    <div class="navbar">
+        <span class="logo">Who's app</span>
+        <span class="chat">chat</span>
+        <span class="signUp">sign up</span>
+    </div>
+</div>
+        <div class="landingPage">
+<div class="login">
+    <div class="topbar">
+    </div>
+    <div class="remain">
+        Create an account
+            <div class="form">
+            <form action="">
+                    <label for="username">Username</label>
+                    <input required type="text" id="username" placeholder="John Doe"> <br>
+                    <label for="password">Password</label>
+                    <input required type="password" id="password" minlength="6" placeholder="********">
+                    <div class="bouton">
+                        <button class="btn btn-primary inscription">S'inscrire</button>
+                    </div>
+             </form>
+            </div>
+            <div class="boutonConnect">
+        </div>
+    </div>
+    
+</div>
+<button class="already btn btn-danger"> J'ai déja un compte</button>
+</div>
+    `
+
+
+boutonEnvoyer.addEventListener("click",()=>{
+
+    requetePost.open("POST", "https://139.162.156.85:8000/messages/4/new", true);
+    requetePost.setRequestHeader('Content-Type', 'application/json');
+    requetePost.send(JSON.stringify({
+        content: newMessage.value
+    }));
+    clearMessageZone(newMessage)
+    refreshPage(200)
+
+
+})
+
+chatButton.addEventListener('click',()=>{
+    display(templateChat)
+    refreshPage(200)
+
+
+})
+
+
+signUpButton.addEventListener('click',()=>{
+
+
+    display(templateSignUp)
+    const inscriptionButton= document.querySelector('.inscription')
+    const alreadyButton = document.querySelector('.already')
+    const createUsername = document.querySelector('#username')
+    const createPassword = document.querySelector('#password')
+
+
+
+    inscriptionButton.addEventListener('click',()=>{
+        console.log(createPassword.value)
+        console.log(createUsername.value)
+
+        fetch("https://139.162.156.85:8000/register"),{
+            method: "POST",
+            body: `{
+    "username":"${createUsername.value}",
+    "password":"${createPassword.value}"
+}`
+        }
+        alert(`Merci de vous être inscrit ${createUsername.value}`)
+        display(templateChat)
+        refreshPage(200)
+
+    })
+    alreadyButton.addEventListener('click',()=>{
+        display(templateChat)
+        refreshPage(200)
+
+    })
+    chatButton.addEventListener('click',()=>{
+        display(templateChat)
+    })
+
+
+})
+
+
+function display(content){
+    clearBodyPage()
+
+    bodyPage.innerHTML=content
+}
+
 function getMessages(){
-         fetch("https://139.162.156.85:8000/messages/")
-            .then(response=>response.json())
-            .then(messages=> {
-                messages.forEach(message=>{
-                    nom=message.author.username
-                    id=message.id
-                    contenu=message.content
-                    templateCard(nom,id,contenu)
+    fetch("https://139.162.156.85:8000/messages/")
+        .then(response=>response.json())
+        .then(messages=> {
+            messages.forEach(message=>{
+                nom=message.author.username
+                id=message.id
+                contenu=message.content
+                templateCard(nom,id,contenu)
 
-                })
             })
-            }
+        })
+}
 
-
+function clearBodyPage(){
+    bodyPage.innerHTML=''
+}
 
 function clearMessageZone(messageZone){
     messageZone.value=""
@@ -71,64 +175,5 @@ async function refreshPage(t){
 }
 
 
-
-boutonEnvoyer.addEventListener("click",()=>{
-
-    requetePost.open("POST", "https://139.162.156.85:8000/messages/4/new", true);
-    requetePost.setRequestHeader('Content-Type', 'application/json');
-    requetePost.send(JSON.stringify({
-        content: newMessage.value
-    }));
-    clearMessageZone(newMessage)
-
-
-})
-
-chatButton.addEventListener('click',()=>{
-    bodyPage.innerHTML = templateChat
-    refreshPage(200)
-
-
-})
-
-
-signUpButton.addEventListener('click',()=>{
-    let templateSignUp=`
-<div class="container page">
-    <div class="navbar">
-        <span class="logo">Who's app</span>
-        <span class="chat">chat</span>
-        <span class="signUp">sign up</span>
-    </div>
-</div>
-        <div class="landingPage">
-<div class="login">
-    <div class="topbar">
-    </div>
-    <div class="remain">
-        Creer un compte de messagerie
-            <div class="form">
-                <form action="">
-                    <label for="username">Username</label>
-                    <input type="text" id="username" placeholder="John Doe"> <br>
-                    <label for="password">Password</label>
-                    <input type="password" id="password" placeholder="********">
-                    <div class="bouton">
-                        <button class="btn btn-primary Connexion" type="submit">S'inscrire</button>
-                    </div>
-                </form>
-            </div>
-        <div class="already"><a href="index.html"> J'ai déja un compte</a></div>
-    </div>
-</div>
-</div>
-    `
-
-    bodyPage.innerHTML= templateSignUp
-})
-
-connexionButton.addEventListener('click',()=>{
-    bodyPage.innerHTML= templateChat
-})
 
 getMessages()
